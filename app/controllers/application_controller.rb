a9613037495e616b394f36b8e43679d55495d0bf
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-08-29 19:01:21 +0300

    commit tree d48cd3ad9ed3141a37e9c111e23e7d6a77974196

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+# app/controllers/application_controller.rb
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+      '<h2>Hello <em>Darkness</em> my old friend!</h2>
+      <h2>Ive come to talk with you again - The Silence</h2>'
+
+    end
+  
+end
